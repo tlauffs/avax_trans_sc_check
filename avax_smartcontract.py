0e016d43a97587b2fd3a76e9c04223b01243f752
tlauffs <timlauffs@gmail.com>
2024-05-14 20:48:08 +0200

    small fixes

--- avax_smartcontract.py
+++ avax_smartcontract.py
@@ -16,7 +16,6 @@ def getTransactionInput(transaction, search_input):
     return False
 
 def getTransactionsSC(contract_address, start_block, end_block, search_input, chunk_size=2048):
-    transaction_counter = 0
     transaction_hashes = []
     try:
         with tqdm() as pbar:
@@ -30,12 +29,11 @@ def getTransactionsSC(contract_address, start_block, end_block, search_input, ch
                 for transaction in transactions:
                     if getTransactionInput(transaction.transactionHash.hex(), search_input):
                         transaction_hashes.append(transaction.transactionHash.hex())
-                        transaction_counter += 1
                 start_block += chunk_size
                 pbar.update(1)
     except Exception as e:
         print("Error:", e)
-    return transaction_hashes, transaction_counter
+    return set(transaction_hashes)
 
 avax_c_w3 = connect()
 start_block = 44900000
@@ -44,6 +42,6 @@ contract_address = "0x215B2ae1E51A43f360901425Edf0e4fDDb30CA80"
 search_input = b'SMOKLM'
 
 print(f'Getting transaction_hashes with {search_input} in input | For contract : {contract_address} | starting block : {44900000} | end block : {end_block}')
-transaction_hashes, transaction_counter =  getTransactionsSC(contract_address, start_block, end_block, search_input)
-print('transaction_hashes: ', transaction_counter)
-print('Amount of transactions:  ', transaction_hashes)
+transaction_hashes =  getTransactionsSC(contract_address, start_block, end_block, search_input)
+print('transaction_hashes: ', transaction_hashes)
+print('Amount of transactions:  ', len(transaction_hashes))
